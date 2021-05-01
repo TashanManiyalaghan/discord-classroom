9fc525fe558da0b5f3ae176489ecb3c979cf96e8
Truman <56100185+trumanbe01@users.noreply.github.com>
2021-05-01 14:57:21 -0400

    Merged functionality of schedule class with the Discord bot to allow for scheduling of events

--- cogs/temp.py
+++ cogs/temp.py
@@ -7,8 +7,10 @@ class Temp(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.schedule = Schedule()
+        # Begin the tasks.loop for ping_event command when the cog is loaded.
         self.ping_event.start()
 
+    # Disable the tasks.loop for ping_event command when the cog is unloaded.
     def cog_unload(self):
         self.ping_event.cancel()
 
@@ -17,18 +19,30 @@ class Temp(commands.Cog):
     async def hello(self, ctx):
         await ctx.send('Hello')
 
+    # Command to create a new event in the Schedule object.
     @commands.command()
-    async def add_event(self, ctx):
-        self.schedule.addEvent('Testing', 2021, 5, 1, 14, 5, 'This is a test')
+    async def add_event(self, ctx, *, params):
+        paramList = params.split()
+        name = paramList[0]
+        date = paramList[1].split('/')
+        time = paramList[2].split(':')
+        desc = ' '.join(paramList[3:])
 
+        event = self.schedule.addEvent(name, int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1]), desc)
+        await ctx.send(f'The following event was created: \n\t{event}')
+
+    # Command to display in the console the events of the Schedule object.
     @commands.command()
-    async def check_events(self, ctx):
-        print(self.schedule)
+    async def view_schedule(self, ctx):
+        await ctx.send(str(self.schedule))
 
+    # Tasks loop that will refresh every minute and ping any events currently taking place.
+    # NOTE: Pinging system for when an event is taking place is still required.
     @tasks.loop(seconds = 60)
     async def ping_event(self):
+        print('The following event(s) are taking place:')
         for event in self.schedule.checkCurrent():
-            print(event)
+            print(f'\t{event}\n')
             self.schedule.removeEvent(event.name)
 
 # Cog setup
--- schedule.py
+++ schedule.py
@@ -26,7 +26,7 @@ class Schedule:
 
         # String function to display the contents of the Event object.
         def __str__(self):
-            return f'{self.datetime.day:02}/{self.datetime.month:02}/{self.datetime.year:04} {self.datetime.hour:02}:{self.datetime.minute:02} {self.name}:\n\t{self.desc}'
+            return f'{self.datetime.day:02}/{self.datetime.month:02}/{self.datetime.year:04} {self.datetime.hour:02}:{self.datetime.minute:02} {self.name}: {self.desc}'
 
     # Constructor for the Schedule class, which will keep track of all the events for a single instance.
     def __init__(self):
@@ -37,6 +37,7 @@ class Schedule:
         event = self.Event(name, year, month, day, hour, minute, desc)
         self.events.append(event)
         self.events.sort()
+        return event
 
     # removeEvent function to remove a pre-existing event from the Schedule object.
     def removeEvent(self, name):
