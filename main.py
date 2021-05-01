ee9edef20e2d478b0793b8027a5bb81b4d5b0711
Truman <56100185+trumanbe01@users.noreply.github.com>
2021-04-30 22:49:23 -0400

    Created basic files

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('./')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
