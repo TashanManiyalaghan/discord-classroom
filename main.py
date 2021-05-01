edf071a1c2557e8966e5b82ebcf2e869edfcfd6c
Truman Be <trumanbe01@gmail.com>
2021-05-01 00:57:03 -0400

    Restructured bot command style

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+# Discord.py and Discord.ext imports
 import discord
-import os
+from discord.ext import commands
 
+# .env setup
+import os
 from dotenv import load_dotenv
-project_folder = os.path.expanduser('./')  # adjust as appropriate
+project_folder = os.path.expanduser('./')
 load_dotenv(os.path.join(project_folder, '.env'))
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '$')
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+# Generic Discord command
+@client.command(aliases = ['hi', 'hey', 'hello'])
+async def hello(ctx):
+    await ctx.send(f'Hello!')
 
 client.run(os.getenv('TOKEN'))
