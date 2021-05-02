4e2130d1b79992a09e1540da6bb3e6a36ca58ab1
Truman <56100185+trumanbe01@users.noreply.github.com>
2021-05-02 15:13:44 -0400

    Added some comments and renamed some variables to match the Notes class

--- cogs/ExtraNotes.py
+++ cogs/ExtraNotes.py
@@ -5,38 +5,34 @@ from Helper import *
 
 class Notes(commands.Cog):
 
+    # Constructor for lecture cog to keep track of the client, as well as a list for lectures
     def __init__(self, client):
         self.client = client
         self.channel = None
-        self.lecDict = []
+        self.noteList = []
 
 
-    #def setChannel(self, channel):
-       # self.channel = channel
-
-
-    # Adds lectures to dictionary :)
+    # Adds notes to the list
     @commands.command()
     async def addNote(self, ctx, *, params):
         paramList = parse_inputs(params)
-        lecNum = paramList[0]
-        lecName = paramList[1]
+        noteNum = paramList[0]
+        noteName = paramList[1]
         link = paramList[2]
         #fileType = paramList[3]
         desc = paramList[4]
-        #stuff = [lecNum,lecName,link,fileType]
-        
-        lecTitle = "Lec #%s: %s" %(lecNum,lecName)
-        Lembed = discord.Embed(title = lecTitle, url = link, description = desc, color = discord.Color.blue())
 
-        self.lecDict.append(Lembed)
+        # Create a Discord embed and save it to the list of notes
+        noteTitle = "Note #%s: %s" %(noteNum,noteName)
+        Nembed = discord.Embed(title = lecTitle, url = link, description = desc, color = discord.Color.blue())
+        self.noteList.append(Lembed)
 
     # Print out all lectures
     @commands.command()
     async def allNotes(self, ctx):
         buttons = [u"\u23EA", u"\u2B05", u"\u27A1", u"\u23E9"] # skip to start, left, right, skip to end
         current = 0
-        msg = await ctx.send(embed=self.lecDict[current])
+        msg = await ctx.send(embed=self.noteList[current])
     
         for button in buttons:
             await msg.add_reaction(button)
@@ -58,17 +54,17 @@ class Notes(commands.Cog):
                         current -= 1
                     
                 elif reaction.emoji == u"\u27A1":
-                    if current < len(self.lecDict)-1:
+                    if current < len(self.noteList)-1:
                         current += 1
 
                 elif reaction.emoji == u"\u23E9":
-                    current = len(self.lecDict)-1
+                    current = len(self.noteList)-1
 
                 for button in buttons:
                     await msg.remove_reaction(button, ctx.author)
 
                 if current != previous_page:
-                    await msg.edit(embed=self.lecDict[current])
+                    await msg.edit(embed=self.noteList[current])
 
 # Cog setup
 def setup(client):
