f9bd2a29414ff9c39b71f4078896804b82062fa8
Truman <56100185+trumanbe01@users.noreply.github.com>
2021-05-02 12:11:28 -0400

    Added comments and tweaked code styling for main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Discord.py and Discord.ext imports
+# Discord.py, Discord.ext, and Classroom class imports
 import discord
 from discord.ext import commands
 from classroom import *
@@ -9,10 +9,11 @@ from dotenv import load_dotenv
 project_folder = os.path.expanduser('./')
 load_dotenv(os.path.join(project_folder, '.env'))
 
-intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True)
-client = commands.Bot(command_prefix = '$', intents=intents)
+# Intents setup and bot initialization
+intents = discord.Intents(messages = True, guilds = True, members = True, reactions = True)
+client = commands.Bot(command_prefix = '$', intents = intents)
 
-# on_ready event, for when the bot logs onto server
+# on_ready event, for when the bot boots
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -42,4 +43,5 @@ for filename in os.listdir('./cogs'):
         # Load only .py cogs and strip the extension
         client.load_extension(f'cogs.{filename[:-3]}')
 
+# Run the bot using the user's token
 client.run(os.getenv('TOKEN'))
